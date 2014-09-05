4b717a8ba4245b12ab50ffd81bc3a96efdaeb320
Laszlo Bako-Szabo <lazics@gmail.com>
2014-09-05 17:38:48 +0300

    using protobuf reflection to gather data

--- proto2dot.py
+++ proto2dot.py
@@ -17,6 +17,7 @@ import traceback
 import StringIO
 import copy
 import pprint
+import re
 
 from google.protobuf import descriptor
 from google.protobuf import reflection
@@ -28,25 +29,19 @@ __license__	= "GPLv3"
 
 class Proto2Dot(object):
 	output = None
-	tree = None
+	files = None
+	messages = None
 
 	def __init__(self, options):
 		self.options = options
 
+		self.messages = {}
 		self.output = {
 			'nodes': {},
 			'connections': [],
 		}
-
-		self.tree = {
-			'name': None,
-			'name_long': None,
-			'label': None,
-			'path': [],
-			'parent': None,
-			'children': {},
-			'links': {},
-		}
+		
+		self.files = {}
 
 		self.field_types_by_value={}
 
@@ -55,122 +50,85 @@ class Proto2Dot(object):
 				continue
 			self.field_types_by_value[ field_descr_v ] = field_descr_n[ 5 : ].lower()
 
-	def find_leaf(self, branch):
-		def _find_leaf(branch):
-			if ( len(branch)==0 ): 
-				return self.tree
-			if ( len(branch)==1 ): 
-				return self.tree['children'][ branch[0] ]
-			else:
-				return _find_leaf( self.tree[ branch[0] ]['children'] )
-		return _find_leaf( copy.deepcopy( branch ) )
+	def field_multiplicity(self, field):
+		if field.label == field.LABEL_REQUIRED:
+			return ("[1..1]", "required" )
+		if field.label == field.LABEL_OPTIONAL:
+			return ("[0..1]", "optional" )
+		if field.label == field.LABEL_REPEATED:
+			return ("[0..n]", "repeated" )
+		raise Exception("Unknown field label");
 
-	def rfind_leaf_by_name(self, branch, name):
-		if name in branch['children']:
-			return branch['children'][name]
+	def check_port_side(self, field):
+		base_prev = re.match('^([^0-9]*)[0-9]*$', self.prev_field_name).group(1)
+		base_cur = re.match('^([^0-9]*)[0-9]*$', field.name).group(1)
+		if base_prev != base_cur:
+			self.port_on_left_side = not self.port_on_left_side
+		self.prev_field_name = field.name
 
-		if branch['parent'] is None:
-			return None
-		if branch['parent']['name'] == name:
-			return branch['parent']
-		else:
-			return self.rfind_leaf_by_name( branch['parent'], name ) 
+	def process_message_class(self, message):
+		#~ logging.debug("processing message class %s" % (message,))
 
-	def process_message_class(self, cls, branch_path):
-		logging.debug("processing message class %s" % (cls,))
+		self.messages[ message.name ] = message
 
-		descr = cls.DESCRIPTOR
-		n = '__'.join( branch_path + [ descr.name ])
-
-		parent_leaf = self.find_leaf( branch_path )
-		leaf = {
-			'name': descr.name,
-			'name_long': n,
-			'label': ' &gt; '.join( branch_path + [ descr.name ]),
-			'path': branch_path + [ descr.name ],
-			'parent': parent_leaf,
-			'children': {},
-			'links': {},
-		}
-		parent_leaf['children'][ descr.name ] = leaf
-
-		self.output["nodes"][ n ]="""
+		self.output["nodes"][ message.name ]="""
 	%(name)s [
 		shape = plaintext
 		label = """ % {
-	"name": n,
+	"name": message.name,
 }
 
-		self.output["nodes"][ n ]+="<<TABLE BORDER=\"0\" CELLBORDER=\"1\" CELLSPACING=\"0\" ALIGN=\"LEFT\" VALIGN=\"TOP\"><TR><TD COLSPAN=\"4\"><B>"+ leaf[ 'label' ] +"</B></TD></TR>"
+		self.output["nodes"][ message.name ]+="<<TABLE BORDER=\"0\" CELLBORDER=\"1\" CELLSPACING=\"0\" ALIGN=\"LEFT\" VALIGN=\"TOP\"><TR><TD COLSPAN=\"4\"><B>"+ message.name +"</B></TD></TR>"
 
-		for field in descr.fields:
-			field_name = field.name
-			self.output["nodes"][ n ] += "<TR>"
+		self.prev_field_name = ''
+		self.port_on_left_side = True
+		for field in message.fields:
+			self.output["nodes"][ message.name ] += "<TR>"
+
+			port = " PORT=\"l_" + field.name + "\""
+			self.check_port_side( field )
 
 			# field number
-			self.output["nodes"][ n ] += "<TD>"+ str(field.number) +"</TD>" 
+			self.output["nodes"][ message.name ] += ("<TD %s>" % ( port if self.port_on_left_side else "", ) )+ str(field.number) +"</TD>" 
 
 			# field multiplicity
-			if field.label == field.LABEL_REQUIRED:
-				mult = '[1..1]'
-				mult_str = "required"
-			if field.label == field.LABEL_OPTIONAL:
-				mult = '[0..1]'
-				mult_str = "optional"
-			if field.label == field.LABEL_REPEATED:
-				mult = '[0..n]'
-				mult_str = "repeated"
-
-			self.output["nodes"][ n ] += "<TD TITLE=\""+ mult_str +"\">" + mult + "</TD>"
+			mult, label = self.field_multiplicity( field )
+			
+			self.output["nodes"][ message.name ] += "<TD TITLE=\""+ label +"\">" + mult + "</TD>"
 
 			# field type
-			self.output["nodes"][ n ] +=	"<TD ALIGN=\"LEFT\"><FONT COLOR=\"#444444\">&lt;" + self.field_types_by_value[ field.type ] + "&gt;</FONT></TD>"
+			self.output["nodes"][ message.name ] +=	"<TD ALIGN=\"LEFT\"><FONT COLOR=\"#444444\">&lt;" + self.field_types_by_value[ field.type ] + "&gt;</FONT></TD>"
 
 			# field name with connection port
-			self.output["nodes"][ n ] +=	"<TD ALIGN=\"LEFT\" PORT=\"l_" + field_name + "\">"
+			self.output["nodes"][ message.name ] +=	"<TD ALIGN=\"LEFT\" %s>" % ( ( port if not self.port_on_left_side else "" ), )
 
 
 			if field.type == field.TYPE_ENUM:
 				# enum field, display a list of values
-				self.output["nodes"][ n ] += "<TABLE BORDER=\"0\" CELLBORDER=\"0\" CELLSPACING=\"0\"  ALIGN=\"LEFT\" VALIGN=\"TOP\"><TR><TD COLSPAN=\"3\" ALIGN=\"LEFT\">"+ field_name +"</TD></TR>"
+				self.output["nodes"][ message.name ] += "<TABLE BORDER=\"0\" CELLBORDER=\"0\" CELLSPACING=\"0\"  ALIGN=\"LEFT\" VALIGN=\"TOP\"><TR><TD COLSPAN=\"3\" ALIGN=\"LEFT\">"+ field.name +"</TD></TR>"
 				for e in field.enum_type.values:
-					self.output["nodes"][ n ] += "<TR><TD WIDTH=\"10\"></TD><TD ALIGN=\"LEFT\"><FONT POINT-SIZE=\""+ str(self.options.font_size - 2) +"\">["+ str(e.number) +"]</FONT></TD><TD ALIGN=\"LEFT\"><I><FONT POINT-SIZE=\""+ str(self.options.font_size - 2) +"\">"+ e.name +"</FONT></I></TD></TR>"
-				self.output["nodes"][ n ] += "</TABLE>"
+					self.output["nodes"][ message.name ] += "<TR><TD WIDTH=\"10\"></TD><TD ALIGN=\"LEFT\"><FONT POINT-SIZE=\""+ str(self.options.font_size - 2) +"\">["+ str(e.number) +"]</FONT></TD><TD ALIGN=\"LEFT\"><I><FONT POINT-SIZE=\""+ str(self.options.font_size - 2) +"\">"+ e.name +"</FONT></I></TD></TR>"
+				self.output["nodes"][ message.name ] += "</TABLE>"
 			else:
 				# not an enum, just display the name
-				self.output["nodes"][ n ] += field_name
-			self.output["nodes"][ n ] += "</TD>"
+				self.output["nodes"][ message.name ] += field.name
+			self.output["nodes"][ message.name ] += "</TD>"
 
 
 			# reference to another message
 			if field.type == field.TYPE_MESSAGE:
-				leaf["links"][ field_name ] = field.message_type.name
+				self.output["connections"].append( "\t\t"+ message.name +":l_"+ field.name + " -> " + field.message_type.name )
 
-			self.output["nodes"][ n ] += "</TR>"
+			self.output["nodes"][ message.name ] += "</TR>"
 
 
-		self.output["nodes"][ n ]+="</TABLE>>"
+		self.output["nodes"][ message.name ]+="</TABLE>>"
 
-		self.output["nodes"][ n ]+="""
+		self.output["nodes"][ message.name ]+="""
 	]
 """ % {
-	"name": n,
+	"name": message.name,
 }
-
-	def generate_links(self, branch = None):
-		if branch is None:
-			branch = self.tree
-		for link_source, link_target in branch['links'].iteritems():
-
-			link_target_leaf = self.rfind_leaf_by_name( branch, link_target )
-
-			if link_target_leaf is None:
-				logging.error("Can't find '%s'" % (link_target,))
-			else:
-				self.output["connections"].append( "\t\t"+ branch['name'] +":l_"+ link_source + " -> " + link_target_leaf["name_long"] )
-
-		for child in branch['children'].itervalues():
-			self.generate_links( child )
 
 	def generate_dot_graph(self):
 		f = StringIO.StringIO()
@@ -204,7 +162,6 @@ digraph protobuf {
 
 		return graph
 
-
 def main():
 	from optparse import OptionParser
 
@@ -220,6 +177,7 @@ def main():
 	parser.add_option( "--doxygen-title", dest="doxygen_title", help="Title for the generated Doxygen page", default="Protocol Buffer Definition Map" )
 	parser.add_option( "-T", dest="dot_output_format", help="Set additional output formats (passed as command-line argument to dot)", type="string", action="append")
 	parser.add_option( "--dot", dest="dot", help="Graphviz directed graph generator (dot)", type="string", default="dot" )
+	parser.add_option( "--plugin", dest="plugin", help="Use a plugin", type="string", default=None )
 	parser.add_option( "-d", "--debug", dest="debug", help="Print debug info", action="store_true", default=False)
 	
 
@@ -231,10 +189,23 @@ def main():
 		parser.print_help()
 		sys.exit(1)
 
+	if options.plugin is not None:
+		options.plugin = os.path.realpath( options.plugin )
+
 	o = Proto2Dot( options )
+
+	if options.plugin is not None:
+		context = {
+			'parser': parser,
+			'o': o,
+			'self': o,
+			'logging': logging,
+		}
+		execfile( options.plugin, context, context )
 
 	proto_dir = os.getcwd()
 
+	error = False
 	for proto_filename in args:
 		proto_filename_ = os.path.abspath( proto_filename )
 		if options.debug:
@@ -272,57 +243,39 @@ def main():
 			m = importlib.import_module( module_name )
 
 			try:
-
-				def _search_for_message_classes(ctx, branch_path = None):
-					if branch_path is None:
-						branch_path = []
-
-					for n in dir(ctx):
-						if n[0]=='_':
-							continue
-						cls = getattr(ctx, n)
-
-						if not isinstance( cls, reflection.GeneratedProtocolMessageType ):
-							continue
-
-						o.process_message_class( cls, branch_path = branch_path )
-
-						_search_for_message_classes(cls, branch_path = branch_path + [ n ])
-
-
-				_search_for_message_classes(m)
+				for message in m.DESCRIPTOR.message_types_by_name.itervalues():
+					o.process_message_class( message )
 
 			except:
 				logging.error("Error processing '%s', skipping: \n%s" % (module_name, traceback.format_exc(),))
+				error = True
+				break
 
-		#~ logging.debug("tree: \n%s" % (pprint.pformat(o.tree),))
-
-		o.generate_links()
-
+	if not error:
 		graph = o.generate_dot_graph()
-
+	
 		# Generate graphviz file
 		dot_filename = proto_filename_+".dot"
-
+	
 		f = open(dot_filename, "wb")
 		f.write( graph )
 		f.close()
-
-
+	
+	
 		if options.dot_output_format:
 			cmd = [ options.dot, '-O', dot_filename ] + [ '-T'+x for x in options.dot_output_format ]
 			logging.debug("executing graphviz: %s" % (cmd,))
-
+	
 			subprocess.call( cmd )
-
-
-
-
+	
+	
+	
+	
 		if options.doxygen:
 			# Generate Doxygen page
 			f = open(proto_filename_+".dox", "wb")
 			f.write("""/**
-\page %(title)s
+\page protobuf_map %(title)s
 
 %(title)s
 ==================================
@@ -339,9 +292,28 @@ def main():
 """)
 			f.close()
 
-		sys.path.remove(tmpdir)
-		if not options.debug:
-			shutil.rmtree(tmpdir)
+		error = False
+		try:
+			if options.plugin is not None and 'plugin_post' in context:
+				context['plugin_post']()
+
+		except:
+			logging.error("Error running plugin: \n%s" % (traceback.format_exc(),))
+			error = True
+
+		if not error:
+			for suffix, content in o.files.iteritems():
+				if options.debug:
+					logging.debug("writing file '%s'...\n", (proto_filename_+"."+suffix,) )
+				f = open(proto_filename_+"."+suffix, "wb")
+				if isinstance(content, (list, tuple) ):
+					content = '\n'.join(content)
+				f.write( content )
+				f.close()
+
+	sys.path.remove(tmpdir)
+	if not options.debug:
+		shutil.rmtree(tmpdir)
 
 if __name__ == "__main__":
 	sys.exit( main() )
